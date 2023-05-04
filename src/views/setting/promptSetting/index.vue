3e35ee42eed2668b3a106ef444c632408d794215
bingryan <bingryan@outlook.com>
2023-05-05 01:15:57 +0800

    feat: 🎸 add search

--- src/views/setting/promptSetting/index.vue
+++ src/views/setting/promptSetting/index.vue
@@ -22,6 +22,7 @@
 				</span>
 
 				<input
+					v-model="search"
 					type="text"
 					placeholder="Search"
 					class="block w-full py-2 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
@@ -149,10 +150,21 @@
 	const promptStore = usePromptStore();
 	const file = ref<FileItem | null>(null);
 
-	const promptList = computed(() => promptStore.getTemplate);
-
 	const visible = ref(false);
 	const newPromptModal = ref(true);
+	const search = ref('');
+	const promptList = computed(() => {
+		const searchValue = search.value.trim();
+		if (searchValue) {
+			const templateList = promptStore.getTemplate;
+			return templateList.filter((template) => {
+				const { name, content } = template;
+				return name.includes(searchValue) || content.includes(searchValue);
+			});
+		}
+		return promptStore.getTemplate;
+	});
+
 	const promptForm = reactive({
 		key: '',
 		name: 'new prompt',
