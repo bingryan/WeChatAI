f9de9338f8c3e1580b5b1fb22f236f5a571c9ed7
bingryan <bingryan@outlook.com>
2023-05-19 08:15:56 +0800

    feat: 🎸 add SvgIcon

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<svg :class="svgClass" aria-hidden="true">
+		<use :xlink:href="symbolId" :fill="color" />
+	</svg>
+</template>
+
+<script lang="ts">
+	import { defineComponent, computed } from 'vue';
+
+	export default defineComponent({
+		name: 'SvgIcon',
+		props: {
+			prefix: {
+				type: String,
+				default: 'icon',
+			},
+			name: {
+				type: String,
+				required: true,
+			},
+			color: {
+				type: String,
+				default: 'currentColor',
+			},
+			svgClass: {
+				type: String,
+				default: 'w-5 h-5',
+			},
+		},
+		setup(props) {
+			const symbolId = computed(() => `#${props.prefix}-${props.name}`);
+			return { symbolId };
+		},
+	});
+</script>
